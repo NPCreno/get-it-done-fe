43e9d598c9afddec004393ffe5d65ccd669c3cac
NPCreno <laurence.divinagracia25@gmail.com>
2025-03-13 03:53:11 +0800

    partial signup

--- src/app/components/LandingPage/LandingPage.tsx
+++ src/app/components/LandingPage/LandingPage.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Login from "./Login";
+import Signup from "./Signup";
 
 export default function LandingPage() {
   return (
     <div className="flex flex-row">
-      <div className="min-w-[50vw] min-h-screen bg-gradient-to-t from-primary-default to-white flex flex-col justify-center items-center"></div>
+      {/* left side */}
+      <div className="min-w-[50vw] min-h-screen bg-gradient-to-t from-primary-300 to-white flex flex-col justify-center items-center">
+        <Signup></Signup>
+        {/* <Login></Login> */}
+      </div>
+      {/* right side */}
       <div className="min-w-[50vw] min-h-screen bg-white flex flex-col justify-center items-center">
         <div className="flex flex-col gap-9 items-center">
           <h1 className="font-rancho text-8xl text-primary-default text-center">
--- src/app/components/LandingPage/Signup.tsx
+++ src/app/components/LandingPage/Signup.tsx
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className="flex flex-col rounded-2xl gap-10 bg-white w-[430px] h-[695px] pl-16 pr-16 items-center justify-center shadow-2xl">
+      {/* Header */}
+      <div className="flex flex-col items-center gap-2">
+        <h1 className="font-lato text-[40px] text-primary-default text-center font-bold h-16">
+          Welcome Back!
+        </h1>
+        <span className="font-lato text-lg text-primary-default text-center font-normal h-6">
+          Enter to start getting things done
+        </span>
+      </div>
+
+      <div className=""></div>
+
+      <div className=""></div>
+
+      <div className=""></div>
+    </div>
+  );
 }
