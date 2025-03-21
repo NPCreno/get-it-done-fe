6646902ca83e2960d4e438b7c3177cfa242e49bc
NPCreno <laurence.divinagracia25@gmail.com>
2025-03-22 01:18:40 +0800

    first row cards

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,9 +3,10 @@ import MainLayout from "@/app/components/MainLayout";
 export default function DashboardPage() {
   return (
     <MainLayout>
-      <div className="main flex justify-center ju w-full">
+      <div className="main flex justify-center w-full gap-5">
         {/* Main Page */}
-        <div className="inside max-w-[1440px] w-full px-4 md:px-8 lg:px-12 mx-auto">
+        <div className="inside max-w-[1440px] w-full px-4 md:px-8 lg:px-12 mx-auto gap-5 flex flex-col">
+          {/* Header */}
           <div className="flex justify-between">
             {/* Left header */}
             <div className="flex flex-col">
@@ -16,7 +17,7 @@ export default function DashboardPage() {
                 Track your tasks and monitor your progress
               </p>
             </div>
-
+            {/* Right header */}
             <div className="flex flex-row gap-[10px] items-end">
               <button
                 className="text-text text-[13px] px-5 bg-white rounded-[10px] h-[35px] flex flex-row items-center 
@@ -37,8 +38,63 @@ export default function DashboardPage() {
               </button>
             </div>
           </div>
+
+          {/* first row cards */}
+          <div className="flex flex-row gap-5 h-[100px] w-full fade-in-delay-2">
+            <FirstRowCard
+              icon="/list-outline.png"
+              header="All Tasks"
+              content="300"
+              delay="fade-in-left-delay-1"
+            />
+            <FirstRowCard
+              icon="/timer-outline.png"
+              header="In Progress"
+              content="15"
+              delay="fade-in-left-delay-2"
+            />
+            <FirstRowCard
+              icon="/folder-open-outline.png"
+              header="All Projects"
+              content="3"
+              delay="fade-in-left-delay-3"
+            />
+            <FirstRowCard
+              icon="/checkbox-outline.png"
+              header="Completed"
+              content="300"
+              delay="fade-in-left-delay-4"
+            />
+          </div>
         </div>
       </div>
     </MainLayout>
+  );
+}
+
+function FirstRowCard({
+  icon,
+  header,
+  content,
+  delay,
+}: {
+  icon: string;
+  header: string;
+  content: string;
+  delay: string;
+}) {
+  return (
+    <div
+      className={`px-6 flex flex-row justify-start items-center gap-8 bg-white rounded-[10px] w-full 
+    hover:shadow-[0px_2px_5.1px_-1px_rgba(0,0,0,0.25)] transition-all duration-300 fade-in-left ${delay}`}
+    >
+      <img src={icon} alt={header} />
+      <div className="flex flex-col justify-start">
+        <span className="text-[#838383] text-base font-bold font-lato">
+          {header}
+        </span>
+        <span className="text-text text-[28px] font-lato">{content}</span>
+      </div>
+    </div>
   );
 }
