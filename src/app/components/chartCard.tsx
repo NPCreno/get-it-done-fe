07b047204ce015c5b56bcb795e5947caad7f0d06
NPCreno <laurence.divinagracia25@gmail.com>
2025-06-16 21:56:54 +0800

    coming soon indicator charts

--- src/app/components/chartCard.tsx
+++ src/app/components/chartCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 export default function ChartCard({
   header,
   delay,
@@ -11,7 +12,16 @@ export default function ChartCard({
       hover:shadow-[0px_2px_5.1px_-1px_rgba(0,0,0,0.25)] transition-all duration-300 fade-in-left ${delay}`}
     >
       <span className="text-text text-[13px] font-lato">{header}</span>
-      <div className="w-full h-full rounded-[10px] bg-background"></div>
+      <div className="w-full h-full rounded-[10px] bg-background flex items-center justify-center flex-col">
+        <Image
+        src={"/svgs/under-development.svg"}
+        height={100}
+        width={100}
+        alt="Coming soon"
+        className="opacity-20"
+        />
+        Coming soon
+        </div>
     </div>
   );
 }
